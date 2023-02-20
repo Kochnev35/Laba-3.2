befc4144ca25549b0728d97d7ce23b886a1c5b62
Kochnev35 <90333939+Kochnev35@users.noreply.github.com>
2023-02-20 10:17:37 +0300

    Add files via upload

---
+++ Лабораторная работа №2 C#/laba2 sharp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace laba2_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размерность массива");
+            int c = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[c];
+            Console.WriteLine("Введите элементы массива");
+            for (int i = 0; i < c; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("\nВведенный массив: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + "; ");
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int k = arr[i];
+                int j = i - 1;
+                while(j>=0 && arr[j] > k)
+                {
+                    arr[j + 1] = arr[j];
+                    arr[j] = k;
+                    j--;
+                }
+            }
+            
+             Console.Write("\nОтсортированный массив: ");
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + "; " );
+            }
+            
+        }
+    }
+}
